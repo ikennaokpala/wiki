1feb12aae109ec88b8bc133225523be3cdd0957e
Ikenna N. Okpala <me@ikennaokpala.com>
2017-09-24 19:57:19 +0100

    Feature: A visitor can search
    This implementation make it possible for a visitor can search articles
    by title and programming language.
    
    This commit also comprises of end to end testing of the visitor's
    journey and unit tests.

--- spec/controllers/articles_controller_spec.rb
+++ spec/controllers/articles_controller_spec.rb
@@ -19,6 +19,18 @@ RSpec.describe ArticlesController, type: :controller do
     it "renders the article's new template" do
       expect(subject).to render_template("articles/index")
     end
+
+    it "returns articles from search" do
+      allow(ArticlesQuery).to receive_message_chain(:new, :get)
+        .and_return([:article])
+      allow(ProgrammingLanguage).to receive(:all)
+        .and_return([:programming_language])
+
+      get :index, params: { title: "title", programming_language_id: 1 }
+
+      expect(assigns(:articles)).to match_array([:article])
+      expect(assigns(:programming_languages)).to match_array([:programming_language])
+    end
   end
 
   describe "GET #new" do
--- spec/views/articles/index.html.erb_spec.rb
+++ spec/views/articles/index.html.erb_spec.rb
@@ -1,11 +1,24 @@
 require "rails_helper"
 
 RSpec.describe "articles/index.html.erb", type: :view do
-  it "renders a list of articles" do
+  before do
     assign(:articles, create_list(:article, 2))
+    assign(:programming_languages, build_list(:programming_language, 2))
+  end
 
+  it "renders a list of articles" do
     render
 
     expect(rendered).to have_selector("ul li", count: 2)
+  end
+
+  it "renders articles search form" do
+    expected_form = "form[action='/articles'][accept-charset=UTF-8][method=get]"
+    render
+
+    expect(rendered).to have_selector(expected_form)
+    expect(rendered).to have_selector("input[placeholder='Type article title']")
+    expect(rendered).to have_selector("select option", count: 3)
+    expect(rendered).to have_button("Search")
   end
 end
