61e5dd640bd7e1c84e0800980eb821d7a6d86e61
Ikenna N. Okpala <me@ikennaokpala.com>
2017-09-24 04:04:14 +0100

    Preparatory: A visitor can create an article
    This commit adds a preparatory feature that allows a visitor to create
    an article.
    
    This implementation is done in partial fulfilment of the client's
    requested feature to list articles and allow visitors access.
    
    This implementation comprises of end to end testing of the visitor's
    journey and unit tests.

---
+++ spec/views/articles/new.html.erb_spec.rb
@@ -0,0 +1,13 @@
+require "rails_helper"
+
+RSpec.describe "articles/new.html.erb", type: :view do
+  it "renders a form for creating an account" do
+    expected_form = "form[action='/articles'][accept-charset=UTF-8][method=post]"
+    render
+
+    expect(rendered).to have_selector(expected_form)
+    expect(rendered).to have_field("Title")
+    expect(rendered).to have_field("Content")
+    expect(rendered).to have_button("Create Article")
+  end
+end
